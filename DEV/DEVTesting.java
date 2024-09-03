986cacfeab66edd06cef4cce52efe7fa20e0b3e5
devunlok <unlokdev@gmail.com>
2024-09-03 10:21:11 +0530

    Adding Testing File for DEV & DSA

---
+++ DEV/DEVTesting.java
@@ -0,0 +1,5 @@
+public class DEVTesting {
+    public static void main(String[] args) {
+        System.out.println("DEV | Testing CODE");
+    }
+}
---
+++ DSA/DSTesting.java
@@ -0,0 +1,7 @@
+package DSA;
+
+public class DSTesting {
+    public static void main(String[] args) {
+        System.out.println("Testing | DSA CODE");
+    }
+}
